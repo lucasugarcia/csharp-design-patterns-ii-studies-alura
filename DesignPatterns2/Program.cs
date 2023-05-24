bd60191020b03f8261a6a04016bcb533c9ecdb96
Lucas Garcia <lucasugarcia@gmail.com>
2023-05-24 05:44:54 -0300

    Versionamento inicial do projeto e implementação do design pattern Factory, isolando a criação de um objeto complexo em uma classe

---
+++ DesignPatterns2/Program.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatterns2.Cap1;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IDbConnection conexao = new ConnectionFactory().GetConnection();
+
+            IDbCommand comando = conexao.CreateCommand();
+            comando.CommandText = "SELECT * FROM tabela;";
+        }
+    }
+}
